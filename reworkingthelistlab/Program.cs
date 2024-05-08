6b7ec22ad4622d046700e6dd8741df032e8b8984
Clare S <Clare.Stock@gmail.com>
2024-05-08 10:10:42 -0400

    corrected calculation

--- reworkingthelistlab/Program.cs
+++ reworkingthelistlab/Program.cs
@@ -1,5 +1,10 @@
 ﻿
 using System.ComponentModel.Design;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Markup;
+
 //**NOTE- I started working on this in another program window which ended up really convoluted and messy with code pointing everywhere
 //so I opened an empty solution and pasted in one little section at a time to test and fix so that I could think more clearly
 //still kind of a work in progress :/
@@ -16,12 +21,14 @@ store.Add("Chocolate Bar", 1.99m);
 store.Add("Nachos", 4.99m);
 store.Add("Chewing Gum", 0.75m);
 
-Console.WriteLine("Hello, Welcome to Clare's Snack Store!\n");
+global::System.Console.WriteLine("Hello, Welcome to Clare's Snack Store!\n");
 Console.WriteLine("These options are available for sale:");
 //Display store items here
 foreach (KeyValuePair<string, decimal> kvp in store)
 {
     Console.WriteLine(kvp);
+    Console.WriteLine($"{kvp.Key, -15}${kvp.Value}");
+    
 }
 
 
@@ -36,7 +43,7 @@ while (runProgram)
 
 
 
-global::System.Console.WriteLine("\nPlease choose an item to purchase:");
+Console.WriteLine("\nPlease choose an item to purchase:");
 
 string userInput = Console.ReadLine().ToLower().Trim();
 
@@ -70,22 +77,23 @@ while (true)
     }
     else
     {
-        Console.WriteLine("that answer is invalid, please type y to continue or n to quit");
+        Console.WriteLine("that answer is invalid, please type y or n only");
     }
 }
 }
-
+decimal Shoppingtotal = 0;
 Console.WriteLine("This is the list of items you have selected.\n");
 //displays shopping list and total of amount 
 foreach (string s in shoppinglist)
 {
-    Console.WriteLine(s);
+    Console.WriteLine($"{s,-15}${store[s]}");
+    Shoppingtotal += store[s];
 
 }
-//this next variable is defined incorrectly and returns an improper total. 
-//researching a way to get only the totals the user selected from the dictionary
-decimal Shoppingtotal = store.Skip(0).Sum(x => x.Value);
-Console.WriteLine($"Your total amount for these items is {Shoppingtotal}");
+
+Console.WriteLine($"Your total amount for these items is {Shoppingtotal}.");
+Console.WriteLine("Thank you for shopping at Clare's Snack Store!");
+
 
 //methods
 static bool IsIteminShop(Dictionary<string, decimal> store, string userInput)
